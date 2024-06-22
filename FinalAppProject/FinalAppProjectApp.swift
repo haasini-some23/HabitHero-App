739462518657b9656999d8f335ffead815eee0d0
Haasini <some.haasini@gmail.com>
2024-06-21 23:48:34 -0500

    Initial Commit

---
+++ FinalAppProject/FinalAppProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalAppProjectApp.swift
+//  FinalAppProject
+//
+//  Created by Lingareddy Some on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalAppProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
